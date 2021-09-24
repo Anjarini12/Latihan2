84eaf7441f2db026aff5f58906a82cf9632593cb
Anjarini12 <anjariniekals@gmail.com>
2021-09-24 08:22:18 +0700

    Commit pertama

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Anjarini Eka
+            Saya tinggal di daerah Jatisampurna
+            dan saya seorang yang mempunyai hobi
+            Membaca";
+    }
+}
